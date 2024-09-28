629536076fac70661172682840eaa2b32632b483
HudsonSMadeira <129692594+HudsonSMadeira@users.noreply.github.com>
2024-09-28 16:58:13 -0300

    retorno

--- javascript/contato.js
+++ javascript/contato.js
@@ -1,24 +1,4 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita o comportamento padrão de redirecionamento imediato
-  
-  // Simulação do envio do formulário
-  fetch(this.action, {
-      method: this.method,
-      body: new FormData(this),
-      headers: { 'Accept': 'application/json' }
-  }).then(response => {
-      if (response.ok) {
-          alert("Mensagem enviada com sucesso!"); // Mostra a mensagem de sucesso
-          window.location.href = "portfolio.html";   // Redireciona para a primeira página (substitua pelo nome correto da sua página)
-      } else {
-          alert("Erro ao enviar a mensagem, tente novamente.");
-      }
-  }).catch(error => {
-      alert("Erro ao enviar a mensagem, tente novamente.");
-  });
-});
-
-/* class FormSubmit {
+class FormSubmit {
     constructor(settings) {
       this.settings = settings;
       this.form = document.querySelector(settings.form);
@@ -86,4 +66,4 @@ document.querySelector('form').addEventListener('submit', function(event) {
     success: "<h1 class='success'>Mensagem enviada!</h1>",
     error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
   });
-  formSubmit.init();*/
+  formSubmit.init();
